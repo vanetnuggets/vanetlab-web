cb729443ebc49a0b4040bd9e410904c54545bc13
Vojtech Fudaly <fud4ly@gmail.com>
2022-10-17 00:53:57 +0200

    whiter glass xd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,8 +29,8 @@ export default {
 
 <style scoped>
   .glass {
-    background: white;
-    opacity: 0.85;
+    background: rgba(255,255,255,0.42);
+    
     border-radius: 16px;
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(8px);
