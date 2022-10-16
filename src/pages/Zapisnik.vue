dbf5ac69f2b86f3fb33efe77b318b73dd0606fe0
Ondrej Martinka <parrot@localhost.localdomain>
2022-10-16 20:48:01 +0200

    refactor zapisnik columns

--- src/pages/Zapisnik.vue
+++ src/pages/Zapisnik.vue
@@ -5,13 +5,17 @@
   <div class="jebo">
     <div class="zap_list">
       <div class="col_left">
-        <div v-for="i in files_l" :key="i.id">
-          <zapisnik-item :file="i.file" :name="i.name" :date="i.date" />
+        <div v-for="(e, i) in files" :key="e.id">
+          <div v-if="i % 2 == 0">
+            <zapisnik-item :file="e.file" :name="e.name" :date="e.date" />
+          </div>
         </div>
       </div>
       <div class="col_right">
-        <div v-for="i in files_r" :key="i.id">
-          <zapisnik-item :file="i.file" :name="i.name" :date="i.date" />
+         <div v-for="(e, i) in files" :key="e.id">
+          <div v-if="i % 2 == 1">
+            <zapisnik-item :file="e.file" :name="e.name" :date="e.date" />
+          </div>
         </div>
       </div>
     </div>
@@ -30,7 +34,6 @@
 import zapisnik_raw from '/src/assets/zapisniky.json'
 import ZapisnikItem from '../components/ZapisnikItem.vue'
 
-
 export default {
   name: "Zapisnik",
   components: {
@@ -38,22 +41,11 @@ export default {
   },
   data() {
     return {
-      files_r: zapisnik_raw,
-      files_l: zapisnik_raw
+      files: zapisnik_raw
     }
   },
   mounted() {
-    this.files_r = []
-    this.files_l = []
-    for(let i in zapisnik_raw) {
-      if (i&1) {
-        this.files_r.push(zapisnik_raw[i]);
-      } else {
-        this.files_l.push(zapisnik_raw[i]);
-      }
-    }
-    this.files_r.sort((a, b) => { return a.id - b.id });
-    this.files_l.sort((a, b) => { return a.id - b.id });
+
   }
 }
 </script>
