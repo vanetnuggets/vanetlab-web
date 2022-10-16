071535542128ab168ec1fa6cbac3f552b9dea054
omartinka <parrot@localhost.localdomain>
2022-10-17 00:03:42 +0200

    move about text to json

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="about fade-in">
-    <div class="title">
-    Co to je vanet ? 🚙<br>
+  <div class="about">
+    <div class="fade-in" v-for="a in about" :key="a.id">
+      <div class="title">
+        {{ a.title }}
+      </div>
+      <div v-if="a.subtitle.length > 0 " class="subtitle"> 
+        {{ a.subtitle }}  
+      </div> 
+      <div class="text">
+        {{ a.text }}
+      </div>
+      <div class="space10"/>
+      <div class="space10"/>
+      <div class="space10"/>
     </div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Eros donec ac odio tempor orci dapibus ultrices. Dui accumsan sit amet nulla facilisi morbi. Dignissim sodales ut eu sem. Urna nec tincidunt 
-    <br>
-    <div class="title">
-    A na co mi je vanet dobry ako?<br>
-    </div>
-    praesent semper feugiat nibh sed pulvinar. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Tellus molestie nunc non blandit massa enim nec. Purus in mollis nunc sed id semper risus in hendrerit. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Amet commodo nulla facilisi nullam vehicula ipsum. Sit amet commodo nulla facilisi nullam vehicula ipsum a arcu. Id interdum velit laoreet id donec ultrices tincidunt. 
-    <div class="title">
-      Dost dobre, dufam ze aj nabuduci tyzden donesies tortu dubak<br>
-    </div>
-    Purus viverra accumsan in nisl nisi scelerisque eu ultrices. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Vel orci porta non pulvinar neque laoreet suspendisse. Cras ornare arcu dui vivamus.
-
   </div>
 </template>
 
 <script>
+import about_raw from '/src/assets/about.json'
+
 export default {
   name: "About",
   mounted() {
@@ -26,6 +28,11 @@ export default {
       this.$store.commit('add_fadein_el', e);
     }
     this.$store.dispatch("handleScroll");
+  },
+  data() {
+    return {
+      about: about_raw
+    }
   }
 }
 </script>
