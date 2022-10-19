233852578b4e8da3e6467f14f82d369c13746803
Vojtech Fudaly <fud4ly@gmail.com>
2022-10-19 16:58:45 +0200

    prettier sprints

--- src/components/SprintItem.vue
+++ src/components/SprintItem.vue
@@ -5,21 +5,23 @@
                 {{name}}
             </p>
         </div>
-        <div class="info">
+        <i class="icon fa fa-cog"></i>
+        <div class="content">
             <p>
                 {{text}}
             </p>
         </div>
     </div>
     <div v-else class="sprint_item right fade-in">
-        <div class="info">
-            <p>
-                {{text}}
-            </p>
-        </div>
         <div class="name">
             <p>
                 {{name}}
+            </p>
+        </div>
+        <i class="icon fa fa-cog"></i>
+        <div class="content">
+            <p>
+                {{text}}
             </p>
         </div>
     </div>
@@ -29,7 +31,7 @@
 <script>
   
   export default {
-    name: "SprintkItem",
+    name: "SprintItem",
     props: {
       name: String,
       text: String,
@@ -39,51 +41,125 @@
 </script>
 
 <style scoped>
-.left .name{
-    width: 25%;
-    height: 80px;
-    border: 1px solid black;
-    border-radius: 50%;
-    float: left;
-    margin-right: 50px;
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+.sprint_item .name {
+  position: absolute;
+  display: inline-block;
+  top: calc(50% - 20px);
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+  color: #001d6e;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  z-index: 1;
 }
 
-.left .info{
-    margin: 0 auto;
-    width: 50%;
-    height: 80px;
-    border: 1px solid black;
+.sprint_item.left .name {
+  right: -130px;
 }
 
-.right .name{
-    margin: 0 auto;
-    width: 25%;
-    height: 80px;
-    border: 1px solid black;
-    border-radius: 50%;
+.sprint_item.right .name {
+  left: -130px;
 }
 
-.right .info{
-    width: 50%;
-    height: 80px;
-    border: 1px solid black;
-    float: left;
-    margin-right: 50px;
+.sprint_item .icon {
+  position: absolute;
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  padding: 9px 0;
+  top: calc(50% - 20px);
+  background: #55f6f6;
+  border: 2px solid #001d6e;
+  border-radius: 40px;
+  text-align: center;
+  font-size: 18px;
+  color: #001d6e;
+  z-index: 1;
 }
 
-.left {
-    margin-left: 20%;
+.sprint_item.left .icon {
+  right: 56px;
 }
 
-.right {
-    margin-right: 20%;
+.sprint_item.right .icon {
+  left: 56px;
+}
+
+.sprint_item .content {
+  padding: 30px 90px 30px 30px;
+  background: #55f6f6;
+  position: relative;
+  border-radius: 0 500px 500px 0;
+}
+
+.sprint_item.right .content {
+  padding: 30px 30px 30px 90px;
+  border-radius: 500px 0 0 500px;
+}
+
+.sprint_item .content h2 {
+  margin: 0 0 10px 0;
+  font-size: 18px;
+  font-weight: normal;
+  color: #001d6e;
+}
+
+.sprint_item .content p {
+  margin: 0;
+  font-size: 16px;
+  line-height: 22px;
+  color: #000000;
+}
+
+.sprint_item.left {
+  left: 0;
+}
+
+.sprint_item.right {
+  left: 50%;
 }
 .sprint_item{
-    width: 90%;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    overflow: hidden;
-    text-align: center;
-    display: flex;
+  padding: 15px 30px;
+  position: relative;
+  background: inherit;
+  width: 50%;
+}
+
+.sprint_item.right::after {
+  left: -8px;
+}
+.sprint_item::after {
+  content: '';
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  top: calc(50% - 8px);
+  right: -8px;
+  background: #001d6e;
+  border: 2px solid #001d6e;
+  border-radius: 16px;
+  z-index: 1;
+}
+
+.sprint_item::before {
+  content: '';
+  position: absolute;
+  width: 50px;
+  height: 2px;
+  top: calc(50% - 1px);
+  right: 8px;
+  background: #001d6e;
+  z-index: 1;
+}
+
+.sprint_item.right::before {
+  left: 8px;
 }
 </style>
--- src/pages/Sprinty.vue
+++ src/pages/Sprinty.vue
@@ -2,7 +2,6 @@
     <div class="bigtitle hline_top fade-in" id="sprinty">
       Zoznam šprintov
     </div>
-    <div class="jebo">
       <div class="sprint_list">
         <div v-for="(e, i) in sprints" :key="e.id">
             <div v-if="i % 2 == 0">
@@ -13,18 +12,31 @@
             </div>
           </div>
       </div>
-    </div>
   </template>
   
   <style scoped>
-  .jebo {
-    display: flex;
-    width: 100%;
-    justify-content: center;
-  }
-  .sprint_list{
-    width: 80%;
-  }
+  *,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+  .sprint_list {
+  position: relative;
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 15px 0;
+}
+.sprint_list::after {
+  content: '';
+  position: absolute;
+  width: 2px;
+  background: #001d6e;
+  top: 0;
+  bottom: 10px;
+  left: 50%;
+  margin-left: -1px;
+}
   </style>>
   
   <script>
