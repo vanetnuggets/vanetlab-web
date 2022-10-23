277356338f21f89552543e5e889725abbf157195
Vojtech Fudaly <fud4ly@gmail.com>
2022-10-23 23:20:19 +0200

    refactor for left/right sprint

--- src/pages/Sprinty.vue
+++ src/pages/Sprinty.vue
@@ -4,12 +4,9 @@
   </div>
     <div class="sprint_list">
       <div v-for="(e, i) in sprints" :key="e.id">
-        <div v-if="i % 2 == 0">
-          <sprint-item :text="e.text" :name="e.name" :date="e.date" :type="'left '+get_theme" />
+        <div>
+          <sprint-item :text="e.text" :name="e.name" :date="e.date" :type=get_type(i) />
         </div>
-      <div v-else>
-        <sprint-item :text="e.text" :name="e.name" :date="e.date" :type="'right '+ get_theme"/>
-      </div>
     </div>  
   </div>
 </template>
@@ -63,6 +60,14 @@
     computed: {
       get_theme() {
         return this.$store.getters.theme
+      }
+    },
+    methods: {
+      get_type(i){
+        if (i%2==0){
+          return 'left '+this.get_theme
+        }
+        return 'right '+this.get_theme
       }
     }
   }
