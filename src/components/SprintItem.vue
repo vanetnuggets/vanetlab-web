4c770b47c6529ec04b5330fb5217fd09bfdea970
ondrej martinka <ondrejmar23@gmail.com>
2022-10-23 11:14:23 +0200

    vojto css refactor

--- src/components/SprintItem.vue
+++ src/components/SprintItem.vue
@@ -31,126 +31,122 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 *,
 *::before,
 *::after {
   box-sizing: border-box;
 }
-.sprint_item .date {
-  position: absolute;
-  display: inline-block;
-  top: calc(50% - 20px);
-  text-align: center;
-  font-size: 14px;
-  font-weight: bold;
-  color: #001d6e;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  z-index: 1;
-}
 
-.sprint_item.left .date {
-  right: -130px;
-}
-
-.sprint_item.right .date {
-  left: -130px;
-}
-
-.sprint_item .icon {
-  position: absolute;
-  display: inline-block;
-  width: 40px;
-  height: 40px;
-  padding: 9px 0;
-  top: calc(50% - 20px);
-  background: #55f6f6;
-  border: 2px solid #001d6e;
-  border-radius: 40px;
-  text-align: center;
-  font-size: 18px;
-  color: #001d6e;
-  z-index: 1;
-}
-
-.sprint_item.left .icon {
-  right: 56px;
-}
-
-.sprint_item.right .icon {
-  left: 56px;
-}
-
-.sprint_item .content {
-  padding: 30px 90px 30px 30px;
-  background: #55f6f6;
-  position: relative;
-  border-radius: 0 500px 500px 0;
-}
-
-.sprint_item.right .content {
-  padding: 30px 30px 30px 90px;
-  border-radius: 500px 0 0 500px;
-}
-
-.sprint_item .content h2 {
-  margin: 0 0 10px 0;
-  font-size: 18px;
-  font-weight: normal;
-  color: #001d6e;
-}
-
-.sprint_item .content p {
-  margin: 0;
-  font-size: 16px;
-  line-height: 22px;
-  color: #000000;
-}
-
-.sprint_item.left {
-  left: 0;
-}
-
-.sprint_item.right {
-  left: 50%;
-}
-.sprint_item{
+.sprint_item {
   padding: 15px 30px;
   position: relative;
   background: inherit;
   width: 50%;
+
+  .date {
+    position: absolute;
+    display: inline-block;
+    top: calc(50% - 20px);
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+    color: #001d6e;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    z-index: 1;
+  }
+
+  .icon {
+    position: absolute;
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    padding: 9px 0;
+    top: calc(50% - 20px);
+    background: #55f6f6;
+    border: 2px solid #001d6e;
+    border-radius: 40px;
+    text-align: center;
+    font-size: 18px;
+    color: #001d6e;
+    z-index: 1;
+  }
+
+  .content {
+    padding: 30px 90px 30px 30px;
+    background: #55f6f6;
+    position: relative;
+    border-radius: 0 500px 500px 0;
+
+    h2 {
+      margin: 0 0 10px 0;
+      font-size: 18px;
+      font-weight: normal;
+      color: #001d6e;
+    }
+    p {
+      margin: 0;
+      font-size: 16px;
+      line-height: 22px;
+      color: #000000;
+    }
+  } 
+
+  &.left {
+    left: 0;
+    .date {
+      right: -130px;
+    }
+    .icon {
+      right: 56px;
+    }
+  }
+
+  &.right {
+    left: 50%;
+    .date {
+      left: -130px;
+    }
+    &::after {
+      left: -8px;
+    }
+    &::before {
+      left: 8px;
+    }
+    .content {
+      padding: 30px 30px 30px 90px;
+      border-radius: 500px 0 0 500px;
+    }
+    .icon {
+      left: 56px;
+    }
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    top: calc(50% - 8px);
+    right: -8px;
+    background: #001d6e;
+    border: 2px solid #001d6e;
+    border-radius: 16px;
+    z-index: 1;
+  }
+  
+  &::before {
+    content: '';
+    position: absolute;
+    width: 50px;
+    height: 2px;
+    top: calc(50% - 1px);
+    right: 8px;
+    background: #001d6e;
+    z-index: 1;
+  }
 }
 
-.sprint_item.right::after {
-  left: -8px;
-}
-.sprint_item::after {
-  content: '';
-  position: absolute;
-  width: 16px;
-  height: 16px;
-  top: calc(50% - 8px);
-  right: -8px;
-  background: #001d6e;
-  border: 2px solid #001d6e;
-  border-radius: 16px;
-  z-index: 1;
-}
-
-.sprint_item::before {
-  content: '';
-  position: absolute;
-  width: 50px;
-  height: 2px;
-  top: calc(50% - 1px);
-  right: 8px;
-  background: #001d6e;
-  z-index: 1;
-}
-
-.sprint_item.right::before {
-  left: 8px;
-}
 </style>
